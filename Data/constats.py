3ce02263030d5dbe992fbf64b1fa9669b174b32d
Artyom Shkuratov <@.>
2025-06-12 12:50:41 +0300

    Добавлены константы ответов

---
+++ Data/constats.py
@@ -0,0 +1,8 @@
+DUPLICATE_MESSAGE =  "Этот логин уже используется. Попробуйте другой."
+MISSING_FIELDS_MESSAGE = "Недостаточно данных для создания учетной записи"
+COURIER_CREATED = {"ok": True}
+COURIER_NOT_FOUND_MESSAGE = "Учетная запись не найдена"
+MISSING_FIELDS_LOGIN_MESSAGE = "Недостаточно данных для входа"
+COURIER_NOT_FOUND_BY_ID_MESSAGE = "Курьера с таким id не существует"
+RESPONSE_WITHOUT_ID = "Недостаточно данных для поиска"
+ORDER_NOT_FOUND = "Заказа с таким id не существует"
